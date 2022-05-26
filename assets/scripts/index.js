d7611de4ec3b9fcef0a9666cd3c7b737d3768438
Tiago Farias <tfarias059@gmail.com>
2022-05-25 21:10:35 -0300

    Adicionei o carrosel a lista de itens

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,3 +1,15 @@
 function whats(){
   window.open(`https://web.whatsapp.com/send?phone=5511948559045`, '_blank')
 }
+
+
+$(document).ready(function() {
+  $('.autoWidth').lightSlider({
+      autoWidth:true,
+      loop:true,
+      onSliderLoad: function() {
+          $('.autoWidth').removeClass('cS-hidden');
+      } 
+  });  
+});
+
