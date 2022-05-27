4bddaefe02b77a12243f2d3c93314c4ca46ec323
Tiago Farias <tfarias059@gmail.com>
2022-05-26 22:13:40 -0300

    Criei o menu mobile e a lógica

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -2,14 +2,37 @@ function whats(){
   window.open(`https://web.whatsapp.com/send?phone=5511948559045`, '_blank')
 }
 
+let btnMobile = document.querySelector('.btn-mobile');
 
+const nav = document.getElementById('nav');
+
+function toggleMenu(e) {
+  if (e.type === 'touchstart') {
+    e.preventDefault()
+  };
+
+  nav.classList.toggle('active');
+}
+
+function closeHamburguer() {
+  if (window.screen.width < 768) {
+    nav.classList.toggle('active');
+  }
+}
+
+btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
+
+
+
+// CÓDIGO DO SLIDER
 $(document).ready(function() {
   $('.autoWidth').lightSlider({
       autoWidth:true,
       loop:true,
       onSliderLoad: function() {
-          $('.autoWidth').removeClass('cS-hidden');
-      } 
+        $('.autoWidth').removeClass('cS-hidden');
+      }
   });  
 });
 
